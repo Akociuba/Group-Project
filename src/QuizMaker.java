77f969e0eaebb8da3ccf56011982b00adcce4ed3
127JFriedman <127jfriedman@frhsd.com>
2025-09-10 08:21:15 -0400

    made stuff

--- src/QuizMaker.java
+++ src/QuizMaker.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class QuizMaker {
+    Scanner scanner = new Scanner(System.in);
     private String question;
+    public static ArrayList<String> questions = new ArrayList<String>();
+    public QuizMaker(String q) {
+        question = q;
+    }
+    public void makeQuestion() {
 
+    }
 }
